cb00674827e159a51ff980f570e482e9d58dcdcc
Rajat Sharma <rajat901cx@gmail.com>
2018-12-31 13:27:40 +0530

    adding home page designed by Dev Jay Rathod minor changes by Dev Rajat Sharma

---
+++ index.php
@@ -0,0 +1,171 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css?family=Acme|Work+Sans" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="styling.css">
+    <title>EBSB</title>
+  </head>
+  <body>
+      <nav  class="navbar navbar-expand-lg navbar-light" style="background-color:#FA6405;">
+  <a class="navbar-brand" href="index.php" style="color: white;"><b>EBSB</b></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php" style="color: white">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Events.php" style="color: white">Events</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="imageGallery.php" style="color: white">Image Gallery</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="VideoGallery.php" style="color: white">Video Gallery</a>
+      </li>
+       <li class="nav-item">
+        <a class="nav-link" href="ImageInsert.php" style="color: white">Upload</a>
+      </li>
+     
+    </ul>
+  </div>
+</nav>
+
+
+
+  <div class="col-md-12">
+    <div class="container-fluid" style="text-align: center; margin-top: 20px; margin-bottom: 20px">
+      <img src="imgs/cover.png">
+    </div>
+  </div>
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="imgs/62.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="imgs/92.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="imgs/82.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    
+      
+        
+  
+    
+  
+
+  <div class="container">
+    <div class="box">
+      <div class="row" style="color:#7FDBFF ">
+        .
+      </div>
+      <div class="row shadebox">
+        <div class="col-md-12" style="background-image: url(imgs/headingbg.jpg); background-size: 1180px;" >
+          <h2><u>About</u></h2>
+        </div>
+      </div>
+      
+      <div class="row shadebox">
+        <div class="col-md-12" style="background-image: url(imgs/bgcolor.jpeg);background-size: 1200px ; ">
+          <p  style="text-align: center; color: white;">Under Ek Bharat Shrestha Bharat Programme (EBSB) of GoI, a series of activities are to be undertaken between the paired states and union territories to promote bonding and understanding between diverse cultures.
+          </p>
+          <p style="text-align: center; color:white;">The idea of a sustained and structured cultural connect between denizens of different regions was mooted by Prime Minister Shri Narendra Modi during the Rashtriya Ekta Divas held on 31st October, 2015, to commemorate the birth anniversary of Sardar Vallabhbhai Patel. Hon’ble Prime Minister propounded that cultural diversity is a joy that ought to be celebrated through mutual interaction & reciprocity between people of different States and UTs so that a common spirit of understanding resonates throughout the country.
+          </p>      
+        </div>     
+      </div>
+    </div>
+  </div>
+
+</br>
+
+<!-- CAROUSEL -->
+
+  
+
+    <!-- THE MOU -->
+</hr>
+</br>
+  <div class="container">
+    <div class="box">
+      <div class="row shadebox" >
+        <div class="col-md-6" style="background-image: url(imgs/bgcolor.jpeg);background-size: 750px 600px">
+          <h2><u>The MoU</u></h2>
+          </br>
+          <p style="color:white;">On 21/01/17 at Dhordo, Gujarat an MoU was signed between the paired states during the National Conference of Central and State/UTs Ministers and Secrataries of Tourism, Culture, Youth Affairs and Sports to embark the journey of Ek Bharat Shrestha Bharat</p>          
+        </div>
+        <div class="col-md-6" style="background-image: url(imgs/headingbg.jpg); background-size: 750px 600px">
+          <img src="imgs/cg.jpg" height="229px"  style="border: 2px solid blue;">
+        </div>
+      </div>
+    </div>
+  </div>
+</br>
+
+  <!-- OBJECTIVES -->
+
+  <div class="container">
+    <div class="box">
+      <div class="row shadebox">
+        <div class="col-md-4" style="background-image: url(imgs/headingbg.jpg);background-size: 750px 600px">
+          <img src="imgs/cg2.jpg" height="285px" style="border: 2px solid blue">
+        </div>
+        <div class="col-md-8" style="background-image: url(imgs/bgcolor.jpeg) ;background-size : 800px 600px;">
+          <h2><u>Objectives</u></h2>
+          </br>
+          <ul style="text-align: left; color: white;">
+            <li>To CELEBRATE the Unity in Diversity of our Nation.</li>
+            <li>To PROMOTE the spirit of national integration through a deep and structured engagement between paired states.</li>
+            <li>To SHOWCASE the rich heritage an culture, customs and traditions.</li>
+            <li>To ESTABLISH long-term engagements.</li>
+            <li>To CREATE an environment which promotes learning between states by sharing best practices and experiences.</li>
+          </ul> 
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- FOOTER -->
+  <br><br><br><br>
+
+
+
+  <nav class="navbar navbar-light" style="background-color: #044989">
+  <div style="color: white ; size:40px;">&copy; EBSB 2018</div>
+</nav>
+ 
+
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
