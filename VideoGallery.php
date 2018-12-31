cf16561046d852e931960894782fdd61b658d8a1
Rajat Sharma <rajat901cx@gmail.com>
2018-12-31 13:32:23 +0530

    VideoGallery page designed by Dev Jay Rathod and major changes by Dev Rajat Sharma

---
+++ VideoGallery.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<!-- LINKS -->
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="styling.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link href="https://fonts.googleapis.com/css?family=Acme|Work+Sans" rel="stylesheet">
+
+<body>
+<!-- Photo Grid -->
+ <div class="navbar navbar-fixed-top navbar-light" style=" background-color: #FA6405;">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#hidenavbar">
+        Show Navbar
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="hidenavbar">
+      <ul class="nav navbar-nav" >
+        <a class="navbar-brand" href="index.php" style="color: white;"><b>EBSB</b></a>
+        <li> <a href="index.php" style="font-family:ubuntu; color: white" > Home </a></li>
+        <li> <a href="events.php" style="font-family:ubuntu; color: white" > Events </a></li>
+        <li> <a href="imageGallery.php" style="font-family:ubuntu; color: white"> Image Gallery </a></li>
+        <li> <a href="VideoGallery.php" style="font-family:ubuntu; color: white"> Video Gallery </a></li>
+        <li> <a href="ImageInsert.php" style="font-family:ubuntu; color: white"> Upload </a></li>
+      </ul>
+    </div>
+  </div>
+
+</br>
+</br>
+</br>
+
+<center><h1 style="font-family: Acme;font-size: 40px; color: white"><u>Video Gallery</u></h1></center>
+</br>
+<div class="bgbox container">
+  <div class="d-flex justify-content-around">
+   <iframe width="280" height="280" src="https://www.youtube.com/embed/GdgC9iM7ft4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen"></iframe>
+
+    <iframe width="280" height="280" src="https://www.youtube.com/embed/Mes5rJUl94E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="280" height="280" src="https://www.youtube.com/embed/L-UZnO6ttjg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="280" height="280" src="https://www.youtube.com/embed/yIfZc-GHPJA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="280" height="280" src="https://www.youtube.com/embed/DB4ZA-e6008" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="280" height="280" src="https://www.youtube.com/embed/uRLh9Pgn1ng" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="280" height="280" src="https://www.youtube.com/embed/1EII2l37dE0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="280" height="280" src="https://www.youtube.com/embed/GdgC9iM7ft4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen"></iframe>
+
+    <iframe width="280" height="280" src="https://www.youtube.com/embed/Mes5rJUl94E" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="280" height="280" src="https://www.youtube.com/embed/L-UZnO6ttjg" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="280" height="280" src="https://www.youtube.com/embed/yIfZc-GHPJA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    
+
+    <iframe width="280" height="280" src="https://www.youtube.com/embed/IslgBO9G6SA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+</div>
+
+<br><br><br><br>
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
